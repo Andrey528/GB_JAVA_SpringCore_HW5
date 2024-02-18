5526cc41fe748c630514750856aeeb540f233b02
Andrew <telepnev.1997@mail.ru>
2024-02-18 13:59:14 +0700

    add annotation

--- src/main/java/com/example/GB_JAVA_SpringCore_HW5/repositories/TaskRepository.java
+++ src/main/java/com/example/GB_JAVA_SpringCore_HW5/repositories/TaskRepository.java
@@ -3,9 +3,11 @@ package com.example.GB_JAVA_SpringCore_HW5.repositories;
 import com.example.GB_JAVA_SpringCore_HW5.models.Task;
 import com.example.GB_JAVA_SpringCore_HW5.models.TaskStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
     public List<Task> findAllByStatus(TaskStatus status);
 }
